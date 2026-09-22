d3dd480f033fc88f617f9112f460f4e124d6d97a
Abel0207 <abelcanas92@gmail.com>
2026-09-22 14:32:46 +0100

    Initial commit

---
+++ lecture3/hello/views.py
@@ -0,0 +1,17 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, "hello/index.html")
+
+def abel(request):
+    return HttpResponse("Olá, Abel")
+
+def kelly(request):
+    return HttpResponse("Olá, kelly")
+
+def great(request, name):
+    return render(request, "hello/greet.html",{
+        "name": name.capitalize()
+    })
